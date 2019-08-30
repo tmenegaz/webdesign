0536e118fe998e7a742ad149a5c8ba6e36fa5872
tmenegaz <tmenegaz77@gmail.com>
2019-08-30 06:37:43 -0300

    script atualizado: comentado o log e clear

--- git-push.sh
+++ git-push.sh
@@ -5,5 +5,6 @@ read msn
 git commit -m "$msn"
 git push origin master
 clear
-git log
-clear
+## descomente as duas linhas abaixo se quiser ver o resultado do cimmit e limpar a tela
+#git log
+#clear

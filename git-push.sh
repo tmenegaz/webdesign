ff642a31a11b9e523c229aeb4e356f4d82ffd37c
tmenegaz <tmenegaz77@gmail.com>
2019-08-30 06:45:25 -0300

    script atualizado: add comentários e contexto

--- git-push.sh
+++ git-push.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+## script para adicionar arquivos, commitar e subir a produção para a conta no github
+## a conta do github foi conectada ao repositório local da seguinte forma.
+## no diretório rais do repositório, neste caso disciplina, faça:
+#git init
+#git remote add origin "https://github.com/<usuário-github>/webdesign"
+
 git add .
 echo "Commite aqui"
 read msn
 git commit -m "$msn"
 git push origin master
 clear
-## descomente as duas linhas abaixo se quiser ver o resultado do cimmit e limpar a tela
+## descomente as duas linhas abaixo se quiser ver o resultado do commit e limpar a tela
 #git log
 #clear
